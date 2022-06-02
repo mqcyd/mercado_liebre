8708949c6a77929a5348f549c06fbe9e78acf976
mqcyd <mqcyd09@gmail.com>
2022-06-01 19:52:26 -0500

    subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(7000, () => {
+    console.log("servidor activo");
+})
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+   });
+
+   app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+   });
+
+   app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+   });
+
